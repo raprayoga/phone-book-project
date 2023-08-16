c56cd5b9f5f19b5b1b72b847e5d8a3d072d631a0
raprayoga <prayogarafie@gmail.com>
2023-08-16 13:37:09 +0700

    fix sample component button

--- src/component/elements/Button/Button.test.tsx
+++ src/component/elements/Button/Button.test.tsx
@@ -8,11 +8,7 @@ describe("Render Button", () => {
   test("sould render button correctly", () => {
     const handleClick = jest.fn()
     const handleClickDisabled = jest.fn()
-    const { rerender } = render(
-      <Button onClick={handleClick} label={"ss"}>
-        {text}
-      </Button>
-    )
+    const { rerender } = render(<Button onClick={handleClick}>{text}</Button>)
 
     const buttonElement = screen.getByText(text)
     expect(buttonElement).toBeInTheDocument()
@@ -21,7 +17,7 @@ describe("Render Button", () => {
     expect(handleClick).toBeCalled()
 
     rerender(
-      <Button isdisabled onClick={handleClickDisabled} label={"sasa"}>
+      <Button isdisabled onClick={handleClickDisabled}>
         {text}
       </Button>
     )
