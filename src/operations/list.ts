c2b22dd4167d31d479eae46bcdbcdb4ea25cdfe4
raprayoga <prayogarafie@gmail.com>
2023-08-19 16:23:43 +0700

    set query

--- src/operations/list.ts
+++ src/operations/list.ts
@@ -2,17 +2,11 @@ import { gql } from "@apollo/client"
 
 export const GET_CONTACT_LIST = gql`
   query GetContactList(
-    $limit: Int = 10
-    $offset: Int
+    $limit: Int = 100
     $where: contact_bool_exp
     $order_by: [contact_order_by!]
   ) {
-    contact(
-      limit: $limit
-      offset: $offset
-      where: $where
-      order_by: $order_by
-    ) {
+    contact(limit: $limit, where: $where, order_by: $order_by) {
       created_at
       first_name
       id
